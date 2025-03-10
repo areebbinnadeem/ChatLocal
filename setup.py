8d0ce0e8418160bb13ea547b082d340a12abe281
areebbinnadeem <m.areebbinnadeem@gmail.com>
2025-03-10 12:39:50 +0500

    setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name='mlproject',
+    name='ChatLocal',
     version='0.0.1',
     author='Areeb',
     author_email='M.AreebBinNadeem@gmail.com',
